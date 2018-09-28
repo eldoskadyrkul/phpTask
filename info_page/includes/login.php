b8ba8eed05ea5dda0a712a57da7fcbd11b92d7aa
eldoskadyrkul <41547811+eldoskadyrkul@users.noreply.github.com>
2018-09-28 18:11:11 +0600

    Update login.php

--- info_page/includes/login.php
+++ info_page/includes/login.php
@@ -1,29 +1,27 @@
 <?php
-error_reporting(E_ALL);
-
-include_once('dbconnection.php');
-
+ini_set('error_reporting', E_ALL);
 $_SESSION['Message'] = '';
 
 
 if (isset($_POST['submit'])) {
-
-	$user_login = $_POST['inputUser'];
-	$user_pwd = md5($_POST['inputPassword']);
-
-	$user_login = stripcslashes($user_login);
-	$user_pwd = stripcslashes($user_pwd);
-	$user_login = mysqli_real_escape_string($con, $user_login);
-	$user_pwd = mysqli_real_escape_string($con, $user_pwd);
-
-	$result = mysqli_query($con, "SELECT * FROM login_user WHERE login_username = '$user_login' AND login_password = '$user_pwd'") or die ("Failed to query database".mysql_error());
-	$row = mysqli_fetch_array($result);
-	if($row['login_username'] == $user_login && $row['login_password'] == $user_pwd) {
-		$_SESSION['Message'] = $Lang['success_auth'];
-		header("admin/admin.php?lang". $Lang['lang']);
+	
+	$username = mysqli_real_escape_string($con, trim($_POST['inputUser']));
+	$pwd = mysqli_real_escape_string($con, trim($_POST['inputPassword']));
+	
+	if (!empty($username) && (!empty($pwd))) {
+		$sql_query = "SELECT `id_user`, `login_username` FROM `login_user` WHERE `login_username` = '$username' AND `login_password` = '$pwd'";
+		$data = mysqli_query($con, $sql_query);
+		if (mysqli_num_rows($data) > 0) {
+			$row = mysqli_fetch_assoc($data);
+			setcookie('userid', $row['id_user'], time() + (60*60*24*30));
+			setcookie('username', $row['login_username'], time() + (60*60*24*30));
+			header("Location: admin/admin.php?lang=". $Lang['lang']);
+		} else {
+			header("Location: index.php?lang=". $Lang['lang']);
+			$_SESSION['Message'] = $Lang['name_error'];
+		}
 	} else {
-		$_SESSION['Message'] = $Lang['name_error'];
-		header("index.php?lang=". $Lang['lang']);
+		$_SESSION['Message'] = $Lang['fields_empty'];
 	}
 }
 ?>
