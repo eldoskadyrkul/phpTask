ca61405f6e6fde2c9c68d1d31d3688b62a6e2c22
eldoskadyrkul <41547811+eldoskadyrkul@users.noreply.github.com>
2018-10-05 21:32:12 +0600

    Update login.php

--- info_page/includes/login.php
+++ info_page/includes/login.php
@@ -1,7 +1,8 @@
 <?php
 ini_set('error_reporting', E_ALL);
 $_SESSION['Message'] = '';
-
+include ("includes/dbconnection.php");
+include ("lang/lang_default.php");
 
 if (isset($_POST['submit'])) {
 	CheckPassword();
@@ -11,29 +12,29 @@ function CheckPassword() {
     include ("includes/dbconnection.php");
     include ("lang/lang_default.php");
     $username = mysqli_real_escape_string($con, $_POST['inputUser']);
-	$pwd = mysqli_real_escape_string($con, $_POST['inputPassword']);
+    $pwd = mysqli_real_escape_string($con, $_POST['inputPassword']);
     $new_pwd = md5($pwd);
-    $sql_query = "SELECT `login_username`, `login_password` FROM `login_user` WHERE `login_username` = '$username' AND `login_password` = '$pwd'";
+    $sql_query = "SELECT `login_username`, `login_password` FROM `login_user` WHERE `login_username` = '$username' AND `login_password` = '$new_pwd'";
     $data = mysqli_query($con, $sql_query);
     $num_rows = mysqli_num_rows($data);
-    
-    if ($num_rows != 0) {
-        while ($row = mysqli_fetch_assoc($data)) {
+    if ($num_rows > 0) {
+        while ($row = mysqli_fetch_array($data)) {
             $db_user = $row['login_username'];
             $db_pass = $row['login_password'];
-        }
-        if ($username == $db_user) {
-            if ($new_pwd == $db_pass) {
-                header('Location:admin/admin.php?lang=ru');
+            if ($username == $db_user) {
+                print_r('equals');
+                if (hash('md5', $pwd) == $db_pass) {
+                    header('Location:admin/admin.php?lang=ru');
+                }
+                else 
+                {
+                    header('Location: index.php?lang=ru');
+                }
             }
-            else 
-            {
-                echo 'Password not match';
-                header('Location: index.php?lang=ru');
-            }
-        } else {
-            $_SESSION['Message'] = $Lang['error'];
         }
+    }
+    else {
+        echo "<script>alert('');</script>";
     }
    
 }
