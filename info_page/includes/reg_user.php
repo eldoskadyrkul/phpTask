b72520792e4e537827223256652bf27814f44fd8
eldoskadyrkul <41547811+eldoskadyrkul@users.noreply.github.com>
2018-11-13 20:18:02 +0600

    Update reg_user.php

--- info_page/includes/reg_user.php
+++ info_page/includes/reg_user.php
@@ -7,9 +7,9 @@ $_SESSION['message'] = '';
 if (isset($_POST['reg'])) {
 	
 	if ($_POST['inputPasswords'] === $_POST['inputPasswordCorrects']) {
-        addLogin();
         addRegister();
         addInfo();
+        addLogin();
     }
 	else {
 		$_SESSION['message'] =  $Lang['error_password'];
@@ -21,14 +21,12 @@ function addLogin() {
     include ("lang/lang_default.php");
     /* объявление переменных */
     $user_login = mysqli_real_escape_string($con, $_POST['inputUsers']);
+    $id_user = mysqli_real_escape_string($con, $_POST['user_id']);
     $id_reg = mysqli_real_escape_string($con, $_POST['reg_id']);
     $id_about = mysqli_real_escape_string($con, $_POST['about_id']);
     $user_pwd = md5($_POST['inputPasswords']);
     /* SQL запрос */
-    $sql2 = "SELECT * FROM register_user";
-    $sql3 = "SELECT * FROM about_user";
-    $sql1 = "INSERT INTO login_user(id_user, login_username, login_password, reg_id, about_id) VALUES (DEFAULT,'$user_login','$user_pwd','$id_reg', '$id_about')";
-    
+    $sql1 = "INSERT INTO login_user(`id_user`, `login_username`, `login_password`, `about_id`, `reg_id`) VALUES ('$id_user', '$user_login','$user_pwd','$id_reg', '$id_about')";
    
     /* проверка SQL запроса */
     if (mysqli_query($con, $sql1) === true) {
@@ -43,14 +41,16 @@ function addLogin() {
 function addRegister() {
     include ("dbconnection.php");
     include ("lang/lang_default.php");
+    
     /* объявление переменных */
     $user_fn = mysqli_real_escape_string($con, $_POST['inputFirstNames']);
     $user_ln = mysqli_real_escape_string($con, $_POST['inputLastnames']);
 	$user_active = mysqli_real_escape_string($con, $_POST['inputActivitys']);
 	$user_brtday = mysqli_real_escape_string($con, $_POST['inputBirthdays']);
     $id_reg = mysqli_real_escape_string($con, $_POST['reg_id']);
+    
     /* SQL запрос */
-    $sql = "INSERT INTO register_user(id_reg, reg_firstname, reg_lastname, reg_activity, reg_brthday) VALUES ($id_reg,'$user_fn','$user_ln', '$user_active', '$user_brtday')";
+    $sql = "INSERT INTO register_user (`id_reg`, `reg_firstname`, `reg_lastname`, `reg_activity`, `reg_brthday`) VALUES ('$id_reg', '$user_fn', '$user_ln', '$user_active', '$user_brtday')";
     /* проверка SQL запроса */
     if (mysqli_query($con, $sql) === true) {
         $_SESSION['message'] = 'error_user';
@@ -66,34 +66,39 @@ function addInfo() {
 	$user_work = mysqli_real_escape_string($con, $_POST['inputWork']);
 	$user_url = mysqli_real_escape_string($con, $_POST['inputUrls']);
 	$user_info = mysqli_real_escape_string($con, $_POST['inputInfo']);
-	$user_email = mysqli_real_escape_string($con, $_POST['inputEmail']);
+    $user_email = mysqli_real_escape_string($con, $_POST['inputEmail']);
     $user_avatars = addslashes($_FILES['inputPhotos']['tmp_name']);
     $user_avatarsName = addslashes($_FILES['inputPhotos']['name']);
     $user_avatars = file_get_contents($user_avatars);
     $user_avatars = base64_encode($user_avatars);
     $id_about = mysqli_real_escape_string($con, $_POST['about_id']);
-    
+
+    if(is_uploaded_file($_FILES["inputPhotos"]["tmp_name"]))
+    {
+        $user_avatarsNameString = move_uploaded_file($_FILES["inputPhotos"]["tmp_name"], $_SERVER['DOCUMENT_ROOT']."/img/".$_FILES["inputPhotos"]["name"]);
+    } else {
+        echo("Ошибка загрузки файла");
+    }
     $user_avatarSizeName = $_FILES['inputPhotos']['size'];
 	$user_avatarErrorName = $_FILES['inputPhotos']['error'];
 		
 	$fileExt = explode('.', $user_avatarsName);
 	$fileActualExt = strtolower(end($fileExt));
 		
-	$allowed = array('jpg', 'png', 'jpeg');
+	$allowed = array('png');
     if (in_array($fileActualExt, $allowed)) {
 		if ($user_avatarErrorName === 0) {
 	       if ($user_avatarSizeName < 100000) {
-               $user_read = file_get_contents($user_avatarsName);
+               $user_read = $user_avatars;
            } else {
-				//$_SESSION['message'] = $Lang['size_error'];
+            echo "<script>alert('Поздравляем Вы загрузили изображение!');</script>";
 			}
         }
     } else {
-		//$_SESSION['message'] = $Lang['image_error'];
     }
     
     /* SQL запрос */
-    $sql2 = "INSERT INTO about_user(id_about, work_about, information_about, url_profile, email, image_url) VALUES ($id_about, '$user_work', '$user_info', '$user_url', '$user_email', '$user_avatars')";
+    $sql2 = "INSERT INTO `about_user` (`id_about`, `work_about`, `information_about`, `url_profile`, `email`, `image_url`, `image_name`) VALUES ('$id_about', '$user_work', '$user_info', '$user_url', '$user_email', '$user_avatars', '$user_avatarsName')";
     
     /* проверка SQL запроса */
     if (mysqli_query($con, $sql2) === true) {
