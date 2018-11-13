8035bda90f6a2becabf8e394b115caf988b4a60a
eldoskadyrkul <41547811+eldoskadyrkul@users.noreply.github.com>
2018-11-13 20:23:54 +0600

    Create header.php

---
+++ info_page/common/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title><?=$Lang['page_title'];?></title>
+        <link href="../css/style.css" rel="stylesheet" type="text/css">
+        <link href="../css/media.css" rel="stylesheet" type="text/css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
